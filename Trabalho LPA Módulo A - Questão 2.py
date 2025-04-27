de6429047b0af91182331e339a34ab95ec3b5afc
Gabriel Fontoura <gabrielFMendonca@gmail.com>
2025-04-27 18:15:15 -0300

    alteração da sintaxe dos trabalhos

--- Trabalho LPA Módulo A - Questão 2.py
+++ Trabalho LPA Módulo A - Questão 2.py
@@ -1,15 +1,15 @@
 total_pedido = 0
 
 print("Bem vindo a loja de gelados do Gabriel Fontoura")
+print("-" * 20, "Cardápio", "-" *21 )
+print("-" * 51 )
+print("---|  Tamanho  |  Cupuaçu (CP)  |  Açaí (AC)   |---") 
+print("---|     P     |    R$  9.00    |  R$ 11.00    |---") 
+print("---|     M     |    R$ 14.00    |  R$ 16.00    |---") 
+print("---|     G     |    R$ 18.00    |  R$ 20.00    |---") 
+print("-" * 51 )
 
 while True:
-    print("-" * 20, "Cardápio", "-" *21 )
-    print("-" * 51 )
-    print("---|  Tamanho  |  Cupuaçu (CP)  |  Açaí (AC)   |---") 
-    print("---|     P     |    R$  9.00    |  R$ 11.00    |---") 
-    print("---|     M     |    R$ 14.00    |  R$ 16.00    |---") 
-    print("---|     G     |    R$ 18.00    |  R$ 20.00    |---") 
-    print("-" * 51 )
 
     precos = {
             "cp": {"p": 9.00, "m": 14.00, "g": 18.00},
@@ -19,7 +19,7 @@ while True:
     sabor = input("Entre com o sabor desejado (CP/AC): ").lower()
     sabor_pedido = 0
 
-    if sabor == "AC":
+    if sabor == "ac":
         sabor_pedido = "Açaí"
     else:
         sabor_pedido = "Cupuaçu"
@@ -31,18 +31,17 @@ while True:
     tamanho = input("Entre com o tamanho desejado (P/M/G): ").lower()
             
     if tamanho not in ["p", "m", "g"]: 
-        print("Tamanho inválido. Tente novamente.")
+        print("Tamanho inválido. Tente novamente.\n")
         continue
 
 
     valor = precos[sabor][tamanho]
     total_pedido += valor
 
-    print(f"\nVocê pediu um {sabor_pedido} tamanho {tamanho.upper()}: R$ {valor:.2f}")
-    print(f"Total parcial: R$ {total_pedido:.2f}")
+    print(f"Você pediu um {sabor_pedido} tamanho {tamanho.upper()}: R$ {valor:.2f}\n")
 
     continuar = input("Deseja pedir mais alguma coisa? (S/N): ").strip().lower()
     if continuar != "s":
-        print(f"O valor total a ser pago: R$ {total_pedido:.2f}")
+        print(f"\nO valor total a ser pago: R$ {total_pedido:.2f}")
         break
 

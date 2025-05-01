3f17898ff1b115d78d99c4377cc755f4768e876f
Gabriel Fontoura <gabrielFMendonca@gmail.com>
2025-04-30 21:53:54 -0300

    codigo refatorado para atender requisitos do trabalho

--- Trabalho LPA Módulo A - Questão 2.py
+++ Trabalho LPA Módulo A - Questão 2.py
@@ -1,4 +1,4 @@
-total_pedido = 0
+total_pedido = 0 # variável para armazenar os valores 
 
 print("Bem vindo a loja de gelados do Gabriel Fontoura")
 print("-" * 20, "Cardápio", "-" *21 )
@@ -19,25 +19,43 @@ while True:
     sabor = input("Entre com o sabor desejado (CP/AC): ").lower()
     sabor_pedido = 0
 
+    # Verificando o sabor
     if sabor == "ac":
         sabor_pedido = "Açaí"
-    else:
+        # Tamanho
+        tamanho = input("Entre com o tamanho desejado (P/M/G): ").lower()
+
+        if tamanho == "p":
+            valor = precos["ac"]["p"]
+        elif tamanho == "m":
+            valor = precos["ac"]["m"]
+        elif tamanho == "g":
+            valor = precos["ac"]["g"]
+        else:
+            print("Tamanho inválido. Tente novamente.\n")
+            continue
+    elif sabor == "cp":
         sabor_pedido = "Cupuaçu"
-        
-    if sabor not in ["cp", "ac"]: 
+        # Tamanho
+        tamanho = input("Entre com o tamanho desejado (P/M/G): ").lower()
+
+        if tamanho == "p":
+            valor = precos["cp"]["p"]
+        elif tamanho == "m":
+            valor = precos["cp"]["m"]
+        elif tamanho == "g":
+            valor = precos["cp"]["g"]
+        else:
+            print("Tamanho inválido. Tente novamente.\n")
+            continue
+    else:
         print(f"Sabor inválido. Tente novamente.\n")
         continue
-            
-    tamanho = input("Entre com o tamanho desejado (P/M/G): ").lower()
-            
-    if tamanho not in ["p", "m", "g"]: 
-        print("Tamanho inválido. Tente novamente.\n")
-        continue
-
 
     valor = precos[sabor][tamanho]
     total_pedido += valor
 
+# saída do código
     print(f"Você pediu um {sabor_pedido} tamanho {tamanho.upper()}: R$ {valor:.2f}\n")
 
     continuar = input("Deseja pedir mais alguma coisa? (S/N): ").strip().lower()

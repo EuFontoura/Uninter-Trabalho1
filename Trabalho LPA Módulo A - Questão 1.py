65d79a397b1ef49b8be7a4233291dc8096b84c04
Gabriel Fontoura <gabrielFMendonca@gmail.com>
2025-04-30 21:44:17 -0300

    código ajustado para atender todos os requisitos do exercício

--- Trabalho LPA Módulo A - Questão 1.py
+++ Trabalho LPA Módulo A - Questão 1.py
@@ -1,24 +1,31 @@
+
 print("Bem vindo a loja do Gabriel Fontoura")
 
+# entrada do valor do produto e quantidade
 valor = int(input("Entre com o valor do produto: "))
 quantidade = int(input("Entre com a quantidade do produto: "))
+
+
+# Calculo do valor total
 total = valor * quantidade
 desconto = 0
+totalComDesconto = total # Total com desconto assume o valor total sem desconto pra caso caia no else
 
 if total >= 10000: 
-    desconto = 0.11
+    desconto = 0.11 # desconto de 11%
     totalComDesconto = total - (total * 0.11)
-    print(f"Total sem desconto: {total}\nTotal com desconto: {totalComDesconto}")
     
 elif total >= 6000: 
-    desconto = 0.07
+    desconto = 0.07 # desconto de 7%
     totalComDesconto = total - (total * 0.07)
-    print(f"Total sem desconto: {total}\nTotal com desconto: {totalComDesconto}")
     
 elif total >= 2500: 
-    desconto = 0.04
+    desconto = 0.04 # desconto de 4%
     totalComDesconto = total - (total * 0.04)
-    print(f"Total sem desconto: {total}\nTotal com desconto: {totalComDesconto}")
     
 else: 
-    print(f"Total sem desconto: {total}\nTotal com desconto: Infelizmente o valor mínimo para desconto não foi atingido")
+    # Não há desconto para valores abaixo de 2500
+    print("Infelizmente o valor mínimo para desconto não foi atingido.")
+
+print(f"O valor SEM desconto: R${total:.2f}")
+print(f"O valor COM desconto: R${totalComDesconto:.2f}")

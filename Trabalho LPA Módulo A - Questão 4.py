1939bcb95966e380c0231a543bd445285f084f9f
EuFontoura <gabrielFMendonca@gmail.com>
2025-05-03 10:44:15 -0300

    inicio do trabalho

---
+++ Trabalho LPA Módulo A - Questão 4.py
@@ -0,0 +1,7 @@
+print("Bem vindo a Livraria do Gabriel Fontoura")
+print("-" * 50)
+print("-" * 15, "MENU PRINCIPAL", "-" * 19)
+opcao = input("Escolha a opção desejada:\n1 - Cadastrar Livro\n2 - Consultar Livro(s)\n3 - Remover Livro\n4 - Sair\n")
+
+lista_livro = []
+id_global = 0
